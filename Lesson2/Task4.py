47ce959d76888b30207f1e30db96c7ddba2f8d4f
EEDolgy <eedolgy@gmail.com>
2020-09-30 13:50:09 +0300

    HomeWork 3

--- Lesson2/Task4.py
+++ Lesson2/Task4.py
@@ -3,5 +3,5 @@ sttr = input('Enter some sentence: ')
 lst = sttr.split()
 
 for i in range(len(lst)):
-    lst[i] = lst[i][:10] if len(lst[i]) > 10 else lst[i]
+    lst[i] = lst[i][:10] if len(lst[i]) > 10 else lst[i] #можно было без if
     print(i, lst[i])
