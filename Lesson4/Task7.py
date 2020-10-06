d76d5625e068e0cddcdfb8c2bbc89ae9f6d1427e
EEDolgy <eedolgy@gmail.com>
2020-10-06 20:00:36 +0300

    some changes in old files

--- Lesson4/Task7.py
+++ Lesson4/Task7.py
@@ -1,8 +1,33 @@
 from math import factorial
 
-def fact(n):
-    for el in range(n+1):
-        yield factorial(el)
+# def fact(n):
+#     for el in range(n+1):
+#         yield factorial(el)
 
-for el in fact(5):
-    print(el)
+# def fact(n):
+#     res = 1
+#     for i in range(1, n + 1):
+#         res *= i
+#         yield res
+
+# for el in fact(5):
+#     print(el)
+
+def func(n):
+    loc_var = 0
+    while True:
+        loc_var += 1
+        yield loc_var
+
+var = func(4)
+
+print(next(var))
+print(next(var))
+print(next(var))
+
+print(next(var))
+print(next(var))
+print(next(var))
+print(next(var))
+print(next(var))
+print(next(var))
